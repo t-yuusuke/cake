c61187780eb4c4a980a61b65d3ec192e95d3c911
EC2 Default User <104186291+t-yuusuke@users.noreply.github.com>
2022-09-05 19:09:30 +0000

    add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,10 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
    resources :users, only: [:index, :show, :edit, :update, :destroy]
-   resources :posts, only: [:new, :index, :create]
+   resources :posts, only: [:new, :index, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
