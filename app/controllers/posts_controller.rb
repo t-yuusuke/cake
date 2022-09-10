74daea45078c3fd088301f78a24762593760ad0d
EC2 Default User <104186291+t-yuusuke@users.noreply.github.com>
2022-09-10 20:25:13 +0000

    add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,9 +17,15 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
-  
+
   def edit
     @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
   end
 
   private
